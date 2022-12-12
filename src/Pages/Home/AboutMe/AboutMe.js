64ade668c4c83ae0dad3bd9c1b9a9c4c3ac5f4fa
Sumankdatta <sumankdatta18@gmail.com>
2022-12-12 22:42:35 +0600

    name added

--- src/Pages/Home/AboutMe/AboutMe.js
+++ src/Pages/Home/AboutMe/AboutMe.js
@@ -9,12 +9,12 @@ const AboutMe = () => {
             <div>
                 <div className="hero min-h-screen ">
                     <div className="hero-content flex-col lg:flex-row-reverse " data-aos="fade-down"
-       data-aos-easing="linear"
-       data-aos-duration="1500">
+                        data-aos-easing="linear"
+                        data-aos-duration="1500">
                         <img className='w-6/12' src={about} alt="" />
                         <div>
 
-                            <p className="py-6 text-white ">Hey! I'm Mehedy Hasan Ador and I love to explore myself. I explore lots of fields. At some moments, my interest grows in the development sector. Last three years I hold this sector and try to explore the fields. <br /><br />
+                            <p className="py-6 text-white ">Hey! I'm Suman Kumar Datta and I love to explore myself. I explore lots of fields. At some moments, my interest grows in the development sector. Last one years I hold this sector and try to explore the fields. <br /><br />
 
                                 There were so many changes that happened to me. I completed two courses fully and one course is running. My passions always help me to survive. As you know, how depressed the sector is! When you are stuck, the code is not working and other issues. But, I never quite. I always hold and try to figure out where is the problem and at the end I will always get success.</p>
 
